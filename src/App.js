e119f33f7b3cb7d0aa0acfe856283ef423947c58
Shivam <devshiva.iit.kgp@gmail.com>
2022-01-11 15:31:52 +0530

    CC_simple todo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
 function App() {
+  const [val, setVal] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const onChange = (event) => setVal((prev_val) => event.target.value);
+  const handleForm = (e) => {
+    e.preventDefault();
+    if (val === "") return;
+    setTodos((todos) => [val, ...todos]);
+    setVal((val) => "");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Todo List</p>
+      <form onSubmit={handleForm}>
+        <input type="text" value={val} onChange={onChange}></input>
+        <button> Add Todo</button>
+      </form>
+      <hr />
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>{todo}</li>
+        ))}
+      </ul>
     </div>
   );
 }
 
 export default App;
+//each item returned by the map is a li-component and hence it is rendered by the react.
