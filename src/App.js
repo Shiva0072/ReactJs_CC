c118ec4748d39919d6111268aa13464bb2dcc53a
Shivam <devshiva.iit.kgp@gmail.com>
2022-01-12 01:09:39 +0530

    CC_movie

--- src/App.js
+++ src/App.js
@@ -1,40 +1,15 @@
-import { useEffect, useState } from "react";
-import Coins from "./Coins";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import Details from "./routes/Details";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [coinPrice, setCoinPrice] = useState(0);
-
-  useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((data) => {
-        // console.log(data);
-        setCoins(data);
-        setLoading(false);
-      });
-  }, []);
-
-  const onChange = (e) => {
-    const val = e.target.value;
-    setCoinPrice(val);
-  };
-
   return (
-    <div>
-      <h1>Coins </h1>
-      {loading ? <strong>Loading..</strong> : ""}
-      <select onChange={onChange}>
-        {coins.map((coin) => (
-          <option key={coin.id} value={coin.quotes.USD.price}>
-            {coin.name} {coin.symbol} : ${coin.quotes.USD.price.toFixed(2)}
-          </option>
-        ))}
-      </select>
-      <hr />
-      {loading ? "" : <Coins price={coinPrice} />}
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/movie/:id" element={<Details />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
